0fe6dfd19a79733896b9a0aa0ff46fd9536f18f5
Liam Monninger <l.mak.monninger@gmail.com>
2022-10-01 13:34:59 -0700

    vue-3 migration.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,31 @@
 import Rellax, { RellaxInstance } from 'rellax'
-import Vue, {
-  DirectiveFunction,
-  PluginFunction,
-  PluginObject,
-  VNode,
-} from 'vue'
+import { VNode, DirectiveHook, Plugin, App } from 'vue'
 
-let _Vue: typeof Vue // tslint:disable-line variable-name
+/**
+ * created - new! This is called before the element's attributes or event listeners are applied.
+ * bind → beforeMount
+ * inserted → mounted
+ * beforeUpdate: new! This is called before the element itself is updated, much like the component lifecycle hooks.
+ * update → removed! There were too many similarities to updated, so this is redundant. Please use updated instead.
+ * componentUpdated → updated
+ * beforeUnmount: new! Similar to component lifecycle hooks, this will be called right before an element is unmounted.
+ * unbind -> unmounted
+ */
+
+let _Vue : App; // tslint:disable-line variable-name
 
 const instanceMap = new WeakMap<HTMLElement, RellaxInstance>()
 
-const inserted: DirectiveFunction = (el, { value }, vm) => {
+const inserted: DirectiveHook = (el, { value }, vm) => {
+
+  console.log("Installing Rellax on an element.", el, value);
+
   if (value === false) {
     return
   }
 
   instanceMap.set(el, new Rellax(el, value));
-  
-  console.log("Installed Rellax on an element.");
+
   
 }
 
@@ -29,11 +37,12 @@ const destroy = (el: HTMLElement) => {
   instance.destroy()
 }
 
-const unbind: DirectiveFunction = (el) => {
+const unbind: DirectiveHook = (el) => {
   destroy(el)
 }
 
-const update: DirectiveFunction = (el, { value }, vm) => {
+const update: DirectiveHook = (el, { value }, vm) => {
+  console.log("Updating el...", el, value);
   destroy(el)
   if (value === false) {
     return
@@ -41,18 +50,20 @@ const update: DirectiveFunction = (el, { value }, vm) => {
   instanceMap.set(el, new Rellax(el, value))
 }
 
-const install: PluginFunction<void> = (InjectedVue: typeof Vue) => {
+const install: Plugin = (InjectedVue) => {
 
   _Vue = InjectedVue
 
   _Vue.directive('rellax', {
-    inserted,
-    update,
-    unbind,
+    mounted : inserted,
+    updated : update,
+    unmounted : unbind,
   })
+
+
 }
 
-const plugin: PluginObject<void> = {
+const plugin: Plugin = {
   install,
 }
 
